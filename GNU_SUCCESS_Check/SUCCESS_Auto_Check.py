048e68ba860556ad22c4975152227fb909c2ff54
Chromatic-Hwi <chromatic_365@naver.com>
2022-05-10 00:44:11 +0900

    220510 자동 로그인 프로그램 정산
      각 pc에서 사용이 가능하려면 selenium을 install 해야 하는데 내 데스크탑에선 python 관련 설치와 환경변수 설정이 꼬여서 pip 명령이 제대로 실행되지 않음.
    
      os 모듈을 통해 cmd를 실행시켜도 봤고, python idle 명령도 해봤지만 pip가 실행되지 않는 것은 동일.
    
      주피터 환경에서는 잘 작동하지만 python 파일 단독으로는 실행이 안되는 상황.

---
+++ GNU_SUCCESS_Check/SUCCESS_Auto_Check.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+f = open('./info.txt', 'r')
+lines = f.readlines()
+info=[]
+for line in lines:
+    line = line.strip()
+    info.append(line)
+f.close()
+
+ID=info[1]
+PW=info[3]
+
+path = "./chromedriver.exe"
+driver = webdriver.Chrome(path)
+driver.implicitly_wait(1)
+
+driver.get('https://accounts.gnu.ac.kr/common/login/login.do?service=https://nerum.gnu.ac.kr/sso/apiTest.jsp')
+driver.maximize_window()
+
+driver.find_element(By.XPATH,'//*[@id="userId"]').send_keys(ID)
+driver.find_element(By.XPATH,'//*[@id="password"]').send_keys(PW)
+driver.find_element(By.XPATH,'//*[@id="content"]/div[1]/div/a').click()
+
+driver.find_element(By.XPATH,'//*[@id="gnb"]/ul/li[1]/a').click()
+
+driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/aside/nav/ul/li[2]/p/a').click()
+driver.find_element(By.XPATH,'/html/body/div[2]/div[2]/aside/nav/ul/li[2]/div/ul/li[1]/a').click()
