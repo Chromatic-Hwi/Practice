2267bf1343497910a10cd0e043ef6358f3446d17
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-26 23:34:51 +0900

    221026 C# 문제 2개
      <제곱수 판별>
    Math.Sqrt();로 제곱수를  구해서 제곱해서 주어진 수와 일치하는지 비교했음. 그런데 알고리즘상으론 문제가 없는데 2문제가 계속 실패해서 봤더니 숫자 범위 때문에 값이 초과되서 그런거였음. long 형식으로 선언후 해결함.
    
      <자릿수 더하기>
    https://learn.microsoft.com/ko-kr/dotnet/api/system.char.getnumericvalue?view=net-6.0
    
    로직상으론 분명히 주어진 숫자를 문자열로 바꿔서 인덱스를 증가시키면서 다시 int형으로 변환된 수를 더해주려는데 이상하게 1이 아니라 49 이런식으로 변환되서 계속 시도했었는데 검색해보고 Char.GetNumericValue 라는 처음 보는 메서드로 해결함. 49 이런 숫자가 유니코드 문자였던 것 같음. 이 유니코드를  부동 소수점 숫자로 변환하는 메서드가 바로 위에 있는 저 메서드.

---
+++ Coding_Test/C#/제곱수/제곱수/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 2;
+        long cal = (long)Math.Sqrt(n);
+        if (n == cal * cal) { answer = 1; }
+        return answer;
+    }
+}
