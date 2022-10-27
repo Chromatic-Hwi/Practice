1536555c754440b2227caa1e6773aa89bc762ff1
Chromatic-Hwi <chromatic_365@naver.com>
2022-10-27 18:40:56 +0900

    221027 C# 문제 풀이
      <자릿수 더하기 재시도>
    char. GetNumericValue 기능을 복습하고 다시 한번 풀어봄.
    
      <몫 구하기>
    예제에서 7을 2로 나누면 3.5가 반환되는게 아니라 정수부인 3만 반환되었음. 처음에 간단하게 코드를 작성하고 틀리면 결과나 보자 생각으로 제출했는데 바로 통과했음. 파이썬과 차이가 이런 부분인 듯 함.

---
+++ Coding_Test/C#/자릿수더하기/자릿수더하기/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n)
+    {
+        int answer = 0;
+        var word = n.ToString();
+        for (int i = 0; i < word.Length; i++)
+        {
+            answer += (int)char.GetNumericValue(word[i]);
+        }
+        return answer;
+    }
+}
