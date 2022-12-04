dfd2276c13e601fab2d6d6d4bf7e52a160ee67a5
Chromatic-Hwi <chromatic_365@naver.com>
2022-12-04 21:31:01 +0900

    221204 1차 전체 통신 작동용

---
+++ IotSystemClass/1204/DB_DHT.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+import serial
+import pymysql
+
+app = Flask(__name__)
+
+ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+ser.flush()
+
+db = pymysql.connect(host='localhost',user='root',password='1234',db='mydb')
+cur = db.cursor()
+
+try:
+    while True:
+        ser.write("1".encode())
+        print("a")
+        if ser.in_waiting > 0:
+            print("b")
+            data = ser.readline().decode('utf-8').rstrip()
+            data_s = data.split(',')
+            
+            if len(data_s) <5:
+                continue
+            if data_s[0] == 'T':
+                tem = data_s[1]
+            if data_s[2] == 'H':
+                hum = data_s[3]
+                
+            cur.execute("INSERT INTO dht(temp,humi) VALUES (%s,%s)", (tem,hum))
+            db.commit()
+            print("temp={}, hum={}".format(tem,hum))
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    db.close()
---
+++ IotSystemClass/1204/flast_DHT.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request
+import pymysql
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    db = pymysql.connect(host='localhost', user='root', password='1234', db='mydb')
+    cur = db.cursor()
+    
+    cur.execute("SELECT * from dht ORDER BY id DESC LIMIT 10")
+    rows = cur.fetchall()
+    
+    db.close()
+    
+    return render_template('flask_dht.html', rows=rows)
+
+if __name__=='__main__':
+    app.run(debug=True, port=7080, host='0.0.0.0')
