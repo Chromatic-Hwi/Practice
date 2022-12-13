34e7ffa4ae15b089632b20f6453876712e3ecdc9
Chromatic-Hwi <chromatic_365@naver.com>
2022-12-14 00:00:15 +0900

    221214 PreWork 함수 수정 및 결합
    - LoadInfo 변수로 불러와서 이후 처리할 예정.
    
    - LoadInfo는 하나의 묶음 리스트로 선언될 것이며, 여기서 인덱스로 필요 정보를 순차적으로 뽑아서 이후 아래 코드들에 적용되도록 하면 완성.

--- Complete/PIE/PreWork.py
+++ Complete/PIE/PreWork.py
@@ -1,6 +1,71 @@
 import pickle
 import os
+import sys
+##############################################################################
+def INFO(YN):
+    if YN.upper() == "Y":
+        M_List = list(input("\nM으로 구분된 설비를 입력해주세요. (ex.M1_1 M1_2 M2 M3 대소문자 구분X) \n>> ").split())
+        for _ in range(len(M_List)):
+            M_List.append(M_List[0].upper())
+            del M_List[0]
 
+        TotalLineList = []
+        TotalLineCnt = []
+        for L in range(len(M_List)):
+            print("\n{} 설비에서 Align으로 구분되는 라인을 공백으로 구분해 입력해주세요. (ex. AA CA SV 대소문자 구분X)".format(M_List[L]))
+            LineList = list(input(">> ").split())
+
+            for _ in range(len(LineList)):
+                LineList.append(LineList[0].upper())
+                del LineList[0]
+                    
+            TotalLineList.append(LineList)
+            TotalLineCnt.append(len(LineList))
+                    
+        TotalCamNumList = []
+        for M in range(len(M_List)):
+            CamNumList=[]
+            for L in TotalLineList[M]:
+                print("\n{} 설비 - {} 라인의 카메라 수를 입력해주세요.".format(M_List[M], L))
+                CamNum = input(">> ")
+                CamNumList.append(CamNum)
+            TotalCamNumList.append(CamNumList)
+                    
+        FinalLineList = []
+        for x in range(len(TotalLineList)):
+            for x2 in TotalLineList[x]:
+                FinalLineList.append(x2)
+                        
+        '''  
+        print(M_List)
+        print(TotalLineList)
+        print(TotalLineCnt)
+        #print(FinalLineList)
+        print(TotalCamNumList)
+        '''
+
+        TotalLineNCam=[]
+        for i in range(len(TotalLineList)):
+            LineNCam = dict(zip(TotalLineList[i], TotalCamNumList[i]))
+            TotalLineNCam.append(LineNCam)
+
+        #print(TotalLineNCam)
+
+        # 이후 코드 실행에 필요한 인자들은 M, M들의 line별 리스트=TotalLineList의 소리스트,  각 line의 cam 수=TotalLineNCam의 소딕셔너리
+        M = len(M_List)
+
+        Result = [M, TotalLineList, TotalLineNCam]
+
+        with open("SetInfo.pkl", 'wb') as f:
+            pickle.dump(Result, f)
+
+        return Result
+                
+    else:
+        print("Exit.")
+        sys.exit()
+
+##############################################################################
 YN = input("셋업 정보 저장 과정 실행 여부를 입력해주세요. (Y/N)\n>> ")
 print()
 
@@ -8,68 +73,17 @@ if YN.upper() == "Y":
     if os.path.isfile('./SetInfo.pkl'):
         print("\n셋업 정보가 새로 생성됩니다. 허용하시면 Y 를 입력해주세요.")
         YN = input("진행 여부 (Y/N)\n>> ")
-        if YN.upper() == "Y":
-            M_List = list(input("\nM으로 구분된 설비를 입력해주세요. (ex.M1_1 M1_2 M2 M3 대소문자 구분X) \n>> ").split())
-            for _ in range(len(M_List)):
-                M_List.append(M_List[0].upper())
-                del M_List[0]
-
-
-            TotalLineList = []
-            TotalLineCnt = []
-            for L in range(len(M_List)):
-                print("\n{} 설비에서 Align으로 구분되는 라인을 공백으로 구분해 입력해주세요. (ex. AA CA SV 대소문자 구분X)".format(M_List[L]))
-                LineList = list(input(">> ").split())
-
-                for _ in range(len(LineList)):
-                    LineList.append(LineList[0].upper())
-                    del LineList[0]
-                
-                TotalLineList.append(LineList)
-                TotalLineCnt.append(len(LineList))
-                
-            TotalCamNumList = []
-            for M in range(len(M_List)):
-                CamNumList=[]
-                for L in TotalLineList[M]:
-                    print("\n{} 설비 - {} 라인의 카메라 수를 입력해주세요.".format(M_List[M], L))
-                    CamNum = input(">> ")
-                    CamNumList.append(CamNum)
-                TotalCamNumList.append(CamNumList)
-                
-            FinalLineList = []
-            for x in range(len(TotalLineList)):
-                for x2 in TotalLineList[x]:
-                    FinalLineList.append(x2)
-                    
-            '''  
-            print(M_List)
-            print(TotalLineList)
-            print(TotalLineCnt)
-            #print(FinalLineList)
-            print(TotalCamNumList)
-            '''
-
-            TotalLineNCam=[]
-            for i in range(len(TotalLineList)):
-                LineNCam = dict(zip(TotalLineList[i], TotalCamNumList[i]))
-                TotalLineNCam.append(LineNCam)
-
-            #print(TotalLineNCam)
-
-            # 이후 코드 실행에 필요한 인자들은 M, M들의 line별 리스트=TotalLineList의 소리스트,  각 line의 cam 수=TotalLineNCam의 소딕셔너리
-            M = len(M_List)
-
-            Result = [M, TotalLineList, TotalLineNCam]
-
-            with open("SetInfo.pkl", 'wb') as f:
-                pickle.dump(Result, f)
+        INFO(YN)
     else:
-        sys.quit()
+        INFO(YN)
         
 elif YN.upper() == "N":
-    with open("SetInfo.pkl", 'rb') as f:
-        Info = pickle.load(f)
-    print("저장된 셋업 정보\n>> ", Info)
-
+    try:
+        with open("SetInfo.pkl", 'rb') as f:
+            Info = pickle.load(f)
+        print("저장된 셋업 정보\n>> ", Info)
+    except FileNotFoundError:
+        print("Error!  불러올 셋업 파일이 없습니다!")
+        print("Exit.")
+        sys.exit()
 
