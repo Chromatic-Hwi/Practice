5873ae9cfd1d3ee41f26ae6f9e2ebbdbf2d7a8eb
hseo <hseo@piegroup.co.kr>
2024-05-07 11:13:18 +0900

    240507 Daisy Analog Input 해결

--- Daisy_Seed/MyMy/08_Pot_Input_Test/08_Pot_Input_Test.ino
+++ Daisy_Seed/MyMy/08_Pot_Input_Test/08_Pot_Input_Test.ino
@@ -7,13 +7,6 @@ float analog_value0;
 float analog_value1;
 float analog_value2;
 float analog_value3;
-float analog_value4;
-
-float analog_knob0;
-float analog_knob1;
-float analog_knob2;
-float analog_knob3;
-float analog_knob4;
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -23,13 +16,7 @@ void setup() {
   hw = DAISY.init(DAISY_SEED, AUDIO_SR_48K);
   num_channels = hw.num_channels;
   sample_rate = DAISY.get_samplerate();
-  
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  
+
   // Check value of pot
   Serial.begin(9600);
 }
@@ -39,12 +26,10 @@ void setup() {
 void loop() {
   analogReadResolution(16); //Not a GPIO Num
 
-  analog_value0 = analogRead(A0);
+  analog_value0 = analogRead(A0); //0~65535
   analog_value1 = analogRead(A1);
   analog_value2 = analogRead(A2);
   analog_value3 = analogRead(A3);
-  analog_value4 = analogRead(A4);
-  //Serial.println(analogRead(A0)); //300~65535
 
   Serial.print(analog_value0);
   Serial.print("  |  ");
